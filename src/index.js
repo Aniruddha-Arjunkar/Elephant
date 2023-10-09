dca955de2dbbf86e34b0596995998252035169cf
Anirudha-Arjunkar <aniruddhaarjunkar@gmail.com>
2023-10-09 08:56:11 +0530

    component and view folder is added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Navbar from "./component/navbar";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
